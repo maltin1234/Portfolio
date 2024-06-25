91d00196f6a123b4807cd63e64bec6f6cafdf4f9
Maltin Coku (External - Experis AB) <maltin.coku@ingka.com>
2024-06-25 17:54:51 +0200

    added project section

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,23 @@
+"use client"
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "./components/NavBar";
 
+const inter = Inter({ subsets: ['latin'] });
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <style jsx global>{`
+          body {
+            font-family: ${inter.style.fontFamily};
+          }
+        `}</style>
+      </head>
       <body>
-        {" "}
-        <NavBar /> {children}
+        <NavBar />
+        {children}
       </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
 "use client";
 import React from "react";
+import Image from "next/image";
 
 const Home = () => {
   return (
-    <div className="bg-gray-900 text-white min-h-screen py-10">
+    <div className="text-white min-h-screen py-10 bg-gray-900">
       <div className="max-w-4xl mx-auto px-4 py-10">
         <section className="mb-16">
           <div className="bg-gray-800 p-8 rounded-lg shadow-lg max-w-3xl mx-auto text-center">
@@ -23,37 +24,46 @@ const Home = () => {
           <div className="absolute border-opacity-20 border-gray-700 h-full border-2 left-1/2 transform -translate-x-1/2"></div>
 
           <div className="mb-8 flex justify-between items-center w-full left-timeline">
-            <div className="order-1 w-5/12"></div>
-            <div className="order-1 w-5/12 p-6 bg-gray-700 rounded-lg shadow-xl">
-              <h2 className="text-xl font-semibold text-white">Price Scraper Project</h2>
-              <p className="mt-2 text-gray-300">Upcoming project...</p>
+            <div className="order-1 w-7/12"></div>
+            <div className="order-1 w-7/12 p-6 bg-gray-700 rounded-lg shadow-xl flex flex-col">
+              <h2 className="text-xl font-semibold text-white">Chrome Extension</h2>
+              <Image src="/assets/Pazter_1.png" alt="Pazter_1" width={400} height={200} className="mt-2 rounded-lg" />
+              <div className="mt-2 text-gray-300 flex-grow overflow-y-auto max-h-40 custom-scrollbar">
+                I started creating my own extension where I use Chrome's API to utilize their context menu to paste my notes that me as a user can create by myself. The reason why I wanted to create it by myself is because I was curious on how extensions are created and published but also for security reasons and customization for my own preference.
+              </div>
               <a href="#" className="text-yellow-300 mt-2 block hover:underline">Learn more</a>
             </div>
           </div>
 
           <div className="mb-8 flex justify-between flex-row-reverse items-center w-full right-timeline">
-            <div className="order-1 w-5/12"></div>
-            <div className="order-1 w-5/12 p-6 bg-gray-700 rounded-lg shadow-xl">
-              <h2 className="text-xl font-semibold text-white">Ecommerce Site</h2>
-              <p className="mt-2 text-gray-300">Upcoming project...</p>
+            <div className="order-1 w-7/12"></div>
+            <div className="order-1 w-7/12 p-6 bg-gray-700 rounded-lg shadow-xl flex flex-col">
+              <h2 className="text-xl font-semibold text-white">System admin manual</h2>
+              <div className="mt-2 text-gray-300 flex-grow overflow-y-auto max-h-40 custom-scrollbar">
+                
+              </div>
               <a href="#" className="text-yellow-300 mt-2 block hover:underline">Learn more</a>
             </div>
           </div>
 
           <div className="mb-8 flex justify-between items-center w-full left-timeline">
-            <div className="order-1 w-5/12"></div>
-            <div className="order-1 w-5/12 p-6 bg-gray-700 rounded-lg shadow-xl">
+            <div className="order-1 w-7/12"></div>
+            <div className="order-1 w-7/12 p-6 bg-gray-700 rounded-lg shadow-xl flex flex-col">
               <h2 className="text-xl font-semibold text-white">Blog Platform</h2>
-              <p className="mt-2 text-gray-300">Upcoming project...</p>
+              <div className="mt-2 text-gray-300 flex-grow overflow-y-auto max-h-40 custom-scrollbar">
+                Upcoming project...
+              </div>
               <a href="#" className="text-yellow-300 mt-2 block hover:underline">Learn more</a>
             </div>
           </div>
 
           <div className="mb-8 flex justify-between flex-row-reverse items-center w-full right-timeline">
-            <div className="order-1 w-5/12"></div>
-            <div className="order-1 w-5/12 p-6 bg-gray-700 rounded-lg shadow-xl">
+            <div className="order-1 w-7/12"></div>
+            <div className="order-1 w-7/12 p-6 bg-gray-700 rounded-lg shadow-xl flex flex-col">
               <h2 className="text-xl font-semibold text-white">Portfolio Site</h2>
-              <p className="mt-2 text-gray-300">Upcoming project...</p>
+              <div className="mt-2 text-gray-300 flex-grow overflow-y-auto max-h-40 custom-scrollbar">
+                Upcoming project...
+              </div>
               <a href="#" className="text-yellow-300 mt-2 block hover:underline">Learn more</a>
             </div>
           </div>
