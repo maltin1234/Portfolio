154f4a931bd2d0ca7cea0ec2d3925a7e87ad8361
Maltin Coku (External - Experis AB) <maltin.coku@ingka.com>
2024-07-30 14:32:43 -0700

    make more readable

--- app/page.js
+++ app/page.js
@@ -7,27 +7,27 @@ const Home = () => {
     {
       title: "Chrome Extension",
       imageSrc: "/assets/Pazter_1.jpg",
-      description: "I started creating my own extension where I use Chrome&apos;s API to utilize their context menu to paste my notes that I, as a user, can create by myself. The reason why I wanted to create it by myself is because I was curious on how extensions are created and published but also for security reasons and customization for my own preference."
+      description: "I started creating my own Chrome extension using Chrome's API to utilize the context menu for pasting notes that I can create myself. I wanted to create this extension to learn how extensions are developed and published, and for security and customization purposes."
     },
     {
-      title: "Sound player project",
+      title: "Sound Player Project",
       imageSrc: "/assets/SoundPlayer.jpg",
-      description: "A smaller project for a support call function for the entrance. The technology used was Next.js with React on a Chrome OS kiosk."
+      description: "This is a smaller project for a support call function at an entrance. The technology used includes Next.js with React, running on a Chrome OS kiosk."
     },
     {
-      title: "Maxico Blog site",
+      title: "Maxico Blog Site",
       imageSrc: "/assets/maxico.jpg",
-      description: "Started as an intern for a company called Maxico. I was the first one and helped create the blog website from scratch using Vue, MongoDB, and Node.js. Later we became a team and we migrated to React instead. The company does not exist today."
+      description: "As an intern at Maxico, I was the first to help create a blog website from scratch using Vue, MongoDB, and Node.js. Later, our team migrated the project to React. Unfortunately, the company no longer exists."
     },
     {
-      title: "System admin manual project",
+      title: "System Admin Manual Project",
       imageSrc: "/assets/Sysadmin_bat.jpg",
-      description: "In my work and daily life, I have a goal to use and utilize the cmd as much as I can. CMD is a powerful tool, and I have created a script where I can use the commands I want without remembering them."
+      description: "I aim to use the command line (cmd) as much as possible in my work and daily life. CMD is a powerful tool, and I've created a script that allows me to use commands without having to remember them all."
     },
     {
-      title: "Finetuning and hosting AI",
+      title: "Fine-tuning and Hosting AI",
       imageSrc: "/assets/llama.jpg",
-      description: "Hosted AI locally on my computer. Trained the Llama2 model on my custom dataset from Hugging Face. Currently working more on this."
+      description: "I hosted an AI locally on my computer and trained the Llama2 model on a custom dataset from Hugging Face. I am currently working on further developments in this area."
     },
   ];
 
@@ -38,10 +38,10 @@ const Home = () => {
           <div className="bg-gray-800 p-8 rounded-lg shadow-lg text-center">
             <h2 className="text-4xl font-extrabold mb-4 text-white">About Me</h2>
             <p className="text-lg text-gray-300 mb-4">
-              Hello! I am <span className="font-semibold">Maltin</span>, a passionate tech geek. I love creating projects on my free time. This website will showcase some of my experiements and projects that I can showcse for you with a contemplation of my cv. I specialize in <span className="font-semibold">system support and maintenance</span> and enjoy <span className="font-semibold">networking security, linux and windows </span>.
+              Hello! I'm <span className="font-semibold">Maltin</span>, a passionate tech enthusiast. I love working on projects in my free time. This website showcases some of my experiments and projects along with my CV. I specialize in <span className="font-semibold">system support and maintenance</span> and enjoy <span className="font-semibold">networking security, Linux, and Windows</span>.
             </p>
             <p className="text-lg text-gray-300">
-              Feel free to explore my projects and get in touch if you&apos;d like to collaborate!
+              Feel free to explore my projects and get in touch if you'd like to collaborate!
             </p>
           </div>
         </section>
@@ -58,7 +58,6 @@ const Home = () => {
               <div className="text-gray-300 flex-grow overflow-y-auto mt-2 max-h-40 custom-scrollbar">
                 {project.description}
               </div>
-              
             </div>
           ))}
         </div>
