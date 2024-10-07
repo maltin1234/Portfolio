f447e79801e1de9313e2d302847740d0a08636ab
Maltin Coku <matinimalmo@gmail.com>
2024-10-07 21:09:50 +0200

    minor fix

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,16 @@
 "use client"
-import { Inter } from "next/font/google";
+
 import "./globals.css";
 import NavBar from "./components/NavBar";
 import Providers from "./providers";
 
-const inter = Inter({ subsets: ['latin'] });
+
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <style jsx global>{`
-          body {
-            font-family: ${inter.style.fontFamily};
-          }
-        `}</style>
+       
       </head>
       <body>
         <NavBar />
--- app/providers/index.js
+++ app/providers/index.js
@@ -1,6 +1,6 @@
 "use client";
 import { SessionProvider } from "next-auth/react";
-import { Toaster } from "react-hot-toast";
+
 
 const Providers = ({ children }) => {
   return (
